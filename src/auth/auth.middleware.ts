c3201f1fc27c56cc627525468739de3f8ab0be00
kangbc-dev <kangbc.dev@gmail.com>
2024-03-29 00:19:56 +0900

    http headers에 전달하던 x-jwt를 access,refresh_token으로 세분화

--- src/auth/auth.middleware.ts
+++ src/auth/auth.middleware.ts
@@ -5,18 +5,29 @@ import {
 } from '@nestjs/common';
 import { NextFunction } from 'express';
 import { AuthService } from './auth.service';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class authMiddleware implements NestMiddleware {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly userService: UserService,
+  ) {}
   async use(req: Request, res: Response, next: NextFunction) {
-    console.log(req.headers);
-    const token = req.headers['x-jwt'] as string;
-    console.log('token');
-    console.log(token);
-    if ('x-jwt' in req.headers) {
+    if ('access_token' in req.headers && 'refresh_token' in req.headers) {
       try {
-        this.authService.tokenVerifier(token);
+        const accessToken = req.headers['access_token'] as string,
+          refreshToken = req.headers['refresh_token'] as string;
+        const accessTokenResult =
+          await this.authService.tokenVerifier(accessToken);
+        if (!accessTokenResult) {
+          const refreshTokenResult =
+            await this.authService.tokenVerifier(refreshToken);
+        }
+        const result = await this.userService.findById({
+          userId: accessTokenResult.id,
+        });
+        req['user'] = result.user;
       } catch (e) {
         console.log(e.message);
         throw new UnauthorizedException('토큰이 유효하지 않습니다.');
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -10,11 +10,14 @@ export class AuthService {
   private readonly logger = new Logger(AuthService.name);
   constructor(private readonly jwtService: JwtService) {}
 
-  tokenGenerator(payload: number): tokenGeneratorOutput {
+  tokenGenerator(payload: number, type?: string): tokenGeneratorOutput {
     try {
       console.log('payload');
       console.log(payload);
-      const token = this.jwtService.sign({ id: payload });
+      const token = this.jwtService.sign(
+        { id: payload },
+        { expiresIn: type === 'refresh' ? '1m' : '1h' },
+      );
       if (!token) {
         throw new Error('토큰을 생성할 수 없습니다.');
       }
@@ -31,11 +34,15 @@ export class AuthService {
     }
   }
 
-  tokenVerifier(token: string) {
-    const tokenCheck = this.jwtService.verify(token); //iat = 발급시간, exp = 만료시간
-    console.log('tokenCheck');
-    // console.log(new Date(tokenCheck.iat * 1000));
-    // console.log(Date.now() > tokenCheck.iat);
-    return tokenCheck;
+  async tokenVerifier(token: string) {
+    try {
+      const tokenCheck = await this.jwtService.verify(token); //iat = 발급시간, exp = 만료시간
+      // console.log(new Date(tokenCheck.iat * 1000));
+      // console.log(Date.now() > tokenCheck.iat);
+      return tokenCheck;
+    } catch (e) {
+      console.log(e);
+      return false;
+    }
   }
 }
