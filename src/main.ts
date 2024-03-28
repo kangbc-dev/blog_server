01e4cd769f26b586eb22a36451443159f0d29121
kangbc-dev <kangbc.dev@gmail.com>
2024-03-28 21:43:02 +0900

    CORS 설정 추가

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: process.env.NODE_ENV === 'prod' ? 'https://yourdomain.com' : '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Accept',
+    credentials: true,
+  });
   await app.listen(3333);
 }
 bootstrap();
