d24420bbd3e7d44f9e52d9894d62fee724710b38
kangbc-dev <kangbc.dev@gmail.com>
2024-03-29 00:20:49 +0900

    쿼리에 콘솔로그 추가 / useGuards Test

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -5,6 +5,8 @@ import {
 } from './dtos/create-account.dto';
 import { UserService } from './user.service';
 import { signInInput, signInOutput } from './dtos/sign-in.dto';
+import { UseGuards } from '@nestjs/common';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Resolver()
 export class UserResolver {
@@ -14,11 +16,19 @@ export class UserResolver {
   createAccount(
     @Args('input') input: createAccountInput,
   ): Promise<createAccountOutput> {
+    console.log('mutation => createAccount activated');
     return this.userService.createAccount(input);
   }
 
   @Query((returns) => signInOutput)
   signIn(@Args('input') input: signInInput): Promise<signInOutput> {
+    console.log('query => signIn activated');
     return this.userService.signIn(input);
+  }
+
+  @UseGuards(AuthGuard) 
+  @Query((returns) => String)
+  hello(): string {
+    return 'hello';
   }
 }
