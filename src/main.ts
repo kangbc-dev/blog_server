c7486c6f1d8d6bd06ff788c0ad0a6ab666d493f2
kangbc-dev <kangbc.dev@gmail.com>
2024-03-29 16:10:31 +0900

    cookie-parser 사용코드 / https 인증 pem 추가

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync('./key.pem'),
+    cert: fs.readFileSync('./cert.pem'),
+  };
+
+  const app = await NestFactory.create(AppModule, { httpsOptions });
   app.enableCors({
-    origin: process.env.NODE_ENV === 'prod' ? 'https://yourdomain.com' : '*',
+    origin: 'https://localhost:3000',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Accept',
     credentials: true,
   });
+  app.use(cookieParser());
   await app.listen(3333);
 }
 bootstrap();
