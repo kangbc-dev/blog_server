54498753549bdb2bd6fc31629a0dadd7d2eee6f1
kangbc-dev <kangbc.dev@gmail.com>
2024-03-29 16:10:58 +0900

    req.headers.cookie 읽어오는 코드 추가

--- src/auth/auth.middleware.ts
+++ src/auth/auth.middleware.ts
@@ -14,25 +14,48 @@ export class authMiddleware implements NestMiddleware {
     private readonly userService: UserService,
   ) {}
   async use(req: Request, res: Response, next: NextFunction) {
-    if ('access_token' in req.headers && 'refresh_token' in req.headers) {
+    console.log('middleware activated');
+    const cookies = req['cookies'];
+    if ('access_token' in cookies && 'refresh_token' in cookies) {
       try {
-        const accessToken = req.headers['access_token'] as string,
-          refreshToken = req.headers['refresh_token'] as string;
+        const access_token = cookies['access_token']['token'];
+        const refresh_token = cookies['refresh_token']['token'];
+        console.log(access_token);
+        console.log(refresh_token);
         const accessTokenResult =
-          await this.authService.tokenVerifier(accessToken);
+          await this.authService.tokenVerifier(access_token);
         if (!accessTokenResult) {
           const refreshTokenResult =
-            await this.authService.tokenVerifier(refreshToken);
+            await this.authService.tokenVerifier(refresh_token);
         }
+        //토큰 만료시 재 지급 하는 코드가 필요함.
         const result = await this.userService.findById({
           userId: accessTokenResult.id,
         });
         req['user'] = result.user;
       } catch (e) {
-        console.log(e.message);
-        throw new UnauthorizedException('토큰이 유효하지 않습니다.');
+        console.log(e);
       }
     }
+    // if ('access_token' in req.headers && 'refresh_token' in req.headers) {
+    //   try {
+    //     const accessToken = req.headers['access_token'] as string,
+    //       refreshToken = req.headers['refresh_token'] as string;
+    //     const accessTokenResult =
+    //       await this.authService.tokenVerifier(accessToken);
+    //     if (!accessTokenResult) {
+    //       const refreshTokenResult =
+    //         await this.authService.tokenVerifier(refreshToken);
+    //     }
+    //     const result = await this.userService.findById({
+    //       userId: accessTokenResult.id,
+    //     });
+    //     req['user'] = result.user;
+    //   } catch (e) {
+    //     console.log(e.message);
+    //     throw new UnauthorizedException('토큰이 유효하지 않습니다.');
+    //   }
+    // }
     next();
   }
 }
