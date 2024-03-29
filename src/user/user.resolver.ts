166af2cdbba5b42f9c6bc6dab4b07705c827b7e0
kangbc-dev <kangbc.dev@gmail.com>
2024-03-29 16:11:59 +0900

    클라이언트에 httpOnly cookie를 전달하기 위한 context 참조

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -1,4 +1,4 @@
-import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Args, Context, Mutation, Query, Resolver } from '@nestjs/graphql';
 import {
   createAccountInput,
   createAccountOutput,
@@ -7,6 +7,7 @@ import { UserService } from './user.service';
 import { signInInput, signInOutput } from './dtos/sign-in.dto';
 import { UseGuards } from '@nestjs/common';
 import { AuthGuard } from 'src/auth/auth.guard';
+import { Response } from 'express';
 
 @Resolver()
 export class UserResolver {
@@ -21,12 +22,16 @@ export class UserResolver {
   }
 
   @Query((returns) => signInOutput)
-  signIn(@Args('input') input: signInInput): Promise<signInOutput> {
+  signIn(
+    @Args('input') input: signInInput,
+    @Context() context: any,
+  ): Promise<signInOutput> {
     console.log('query => signIn activated');
-    return this.userService.signIn(input);
+    const res: Response = context.res;
+    return this.userService.signIn(input, res);
   }
 
-  @UseGuards(AuthGuard) 
+  // @UseGuards(AuthGuard)
   @Query((returns) => String)
   hello(): string {
     return 'hello';
