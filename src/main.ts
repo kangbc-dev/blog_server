80355b520808f44fb4826ffea30982ad3ae0f1cf
kangbc-dev <kangbc.dev@gmail.com>
2024-04-05 23:18:44 +0900

    cors origin url 변경

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,7 @@ async function bootstrap() {
 
   const app = await NestFactory.create(AppModule, { httpsOptions });
   app.enableCors({
+    // origin: ['https://localhost:3000', 'https://121.152.110.116:3000'],
     origin: 'https://localhost:3000',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Accept',
